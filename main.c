c1258321927f9d2a5dfed38b40dd628c6bc74dd5
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-09-21 13:15:38 +0100

    program initial state (prints lines of a file and handle errors for usage and inaccessible file)

---
+++ main.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ * main - entry point
+ * @argc: arguments count
+ * @argv: list of arguments
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	char line[256];
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	
+
+    while (fgets(line, sizeof(line), file)) {
+        /* note that fgets don't strip the terminating \n, checking its
+           presence would allow to handle lines longer that sizeof(line) */
+        printf("%s", line); 
+    }
+	return (0);
+}
